ba925ec64ea7e7ef98ac70762e01f0dde48dde4d
nick <noobping@users.noreply.github.com>
2026-08-15 18:14:18 +0200

    mime

---
+++ crates/keycord-passkey/src/mime.rs
@@ -0,0 +1,4 @@
+pub const PASSKEY_MIME_PACKAGE: &str =
+    include_str!("../data/io.github.noobping.keycord-passkey.xml");
+pub const PASSKEY_MIME_TYPES: &str =
+    "application/vnd.keycord.passkey-request+json;application/vnd.keycord.passkey+json;";
